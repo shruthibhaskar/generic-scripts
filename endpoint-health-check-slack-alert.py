f4916300293ba722ad2598f9bee28a4be910afad
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2023-11-09 10:32:52 +0800

    Update endpoint-health-check-slack-alert.py removed sensitive information

--- endpoint-health-check-slack-alert.py
+++ endpoint-health-check-slack-alert.py
@@ -1,7 +1,7 @@
 import requests
 
 # Set variables
-endpoint = "http://10.40.76.4:3128"
+endpoint = "health-check-endpoint"
 slack_webhook_url = "https://hooks.slack.com/services/dummy/dummy"
 channel = "#alert-test"
 botname = "Health Check Bot"
@@ -17,7 +17,7 @@ def send_slack_message(message):
 
 # Print initial message with formatting
 print("-----------------------------")
-print("Initiating health check for the stg intranet proxy...")
+print("Initiating health check for the ednpoint...")
 print("-----------------------------")
 
 try:
@@ -28,17 +28,17 @@ try:
     # Check the response status and send detailed messages
     if status_code == 200:
         print("-----------------------------------------")
-        print("Health check completed for stg intranet proxy.\nStatus code: {}".format(status_code))
+        print("Health check completed for ednpoint.\nStatus code: {}".format(status_code))
         print("-----------------------------------------")
-        send_slack_message("🟢 The stg intranet proxy is up and running. Status code: {}. Augustine, please review the script and provide feedback if necessary.".format(status_code))
+        send_slack_message("🟢 The endpoint is up and running. Status code: {}")
     else:
         print("-----------------------------------------")
-        print("Health check completed for stg intranet proxy.\nStatus code: {}".format(status_code))
+        print("Health check completed for endpoint.\nStatus code: {}".format(status_code))
         print("-----------------------------------------")
-        send_slack_message("🔴 The stg intranet proxy is not returning 200. Status code: {}. Augustine, please review the script and provide feedback if necessary.".format(status_code))
+        send_slack_message("🔴 The endpoint is not returning 200. Status code: {}")
 
 except requests.RequestException as e:
     print("-----------------------------")
-    print("Failed to reach the stg intranet proxy.")
+    print("Failed to reach the endpoint.")
     print("-----------------------------")
-    send_slack_message("❗ Failed to reach the stg intranet proxy. Augustine, please review the script and provide feedback if necessary.")
+    send_slack_message("❗ Failed to reach the endpoint")
